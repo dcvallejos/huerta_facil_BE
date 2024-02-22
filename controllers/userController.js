1ce00be1700598882f11d1cee528651bb64076f3
Lorena Aylen <lag9512@gmail.com>
2024-02-22 20:27:40 -0300

    feat: Add success and fail message to register

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,38 @@
 // llamar a la conexión 
-
+const sql = require('../connection.js')
 
 
 const userController = {
   'login': function (req, res) {
 
   },
-  'createUser': function (req, res) {
+  'createUser': async function (req, res) {
 
-    const send = {
-      "data": {}
+    const send = {}
+    // 
+    const usuario = req.body.usuario,
+          provincia = req.body.provincia,
+          password = req.body.password;
+
+  
+   // const data = await sql`SELECT createUser(${usuario}, ${provincia}, ${password})`
+
+    const test = await sql`SELECT * FROM usuarios WHERE usuario = ${usuario}`
+    if(test.length >= 1){
+      send.errors = []
+      const err = {
+        "status": 409,
+        "title": "Conflict",
+        "message": "Email en uso. Utilice otro"
+      }
+      send.errors.push(err)
+      res.send(send)
+    } else {
+      send.data = {}
+      send.data.message = 'Usuario registrado'
+      res.send(send)
     }
-    res.send('hola')
+    
   },
   'getFavs': function (req, res) {
     
