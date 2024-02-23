ad8dc158ca5ba772847ebb9e15f7a07ce4010ec6
Lorena Aylen <lag9512@gmail.com>
2024-02-22 23:16:50 -0300

    fix: Names

--- routes/plants.js
+++ routes/plants.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const router = express.Router()
-const {getFilters, getPlantById, getAllCards, filterBy } = require('../controllers/plantsController')
+const {getFilters, getPlantById, getCards, filterBy } = require('../controllers/plantsController')
 const { validationResult } = require('express-validator')
 const validators = require('../utils/plantVal.js')
 
 router.get('/getFilters', getFilters)
+
 router.get('/getPlantById/:id', validators, function(req, res){
 const invalid = validationResult(req)
+console.log(invalid)
 if(invalid.errors.length > 0){
   res.send(invalid)
 } else {
   getPlantById(req, res)
 }
 })
-router.get('/getAllCards', getAllCards)
+router.get('/getCards', getCards)
 
 router.post('/filterBy', filterBy)
 
