e266c818a3d36a901f3bdf37f4e104a47ba250b9
Daniel Vallejos <dcvallejos@outlook.com>
2024-02-23 15:14:37 -0300

    Agregados validadores customizados para setFav

--- utils/favsVal.js
+++ utils/favsVal.js
@@ -2,15 +2,27 @@ const { param } = require('express-validator')
 
 const validations = {
   id_usuario :  {
+    custom:{
+      options: val =>{
+        return Number.isInteger(val)
+      },
+      errorMessage : 'El valor tiene que ser en formato numérico'
+    },
     notEmpty: {
       errorMessage: 'Campo obligatorio de Id de especie sin completar',
       bail: true
     },
     isInt: {
       errorMessage: 'Ingrese un numero correcto del Id del usuario'
-    }    
+    }
   },
   id_especie:{
+    custom:{
+      options: val =>{
+        return Number.isInteger(val)
+      },
+      errorMessage : 'El valor tiene que ser en formato numérico'
+    },
     notEmpty: {
       errorMessage: 'Campo obligatorio de Id de especie sin completar',
       bail: true
