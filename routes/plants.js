2902e926c88424b4a9a58aaf5e0f7f9646e06955
Lorena Aylen <lag9512@gmail.com>
2024-02-22 21:39:20 -0300

    feat: Add SQL query, sucess, fail

--- routes/plants.js
+++ routes/plants.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const {getFilters, getPlantsById, getAllCards, filterBy } = require('../controllers/plantsController')
+const {getFilters, getPlantById, getAllCards, filterBy } = require('../controllers/plantsController')
 
 router.get('/getFilters', getFilters)
-router.get('/getPlantsById/:id', getPlantsById)
+router.get('/getPlantById/:id', getPlantById)
 router.get('/getAllCards', getAllCards)
 
 router.post('/filterBy', filterBy)
